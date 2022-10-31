15ea5a5ebe2a7dc6e0dbccfd00c2435c3dcf5251
Vladimir <vejin.vladimir@gmail.com>
2022-10-31 15:03:30 +0100

    Create constructors, getters and setters for Course and Student

--- StudentAndCourse/src/Course.java
+++ StudentAndCourse/src/Course.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Course {
+public class Course {
+    private String nameCourse;
+    private String grade;
+
+    public Course(){}
+
+    public Course(String nameCourse, String grade) {
+        this.nameCourse = nameCourse;
+        this.grade = grade;
+    }
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "nameCourse='" + nameCourse + '\'' +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
 }
--- StudentAndCourse/src/Student.java
+++ StudentAndCourse/src/Student.java
@@ -1,3 +1,52 @@
+import java.util.Arrays;
+
 public class Student {
-    
+
+    private String name;
+
+    private String studentId;
+
+    private Course[] courses;
+
+    public Student() {
+    }
+
+    public Student(String name, String studentId, Course[] courses) {
+        this.name = name;
+        this.studentId = studentId;
+        this.courses = courses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public Course[] getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Course[] courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentId='" + studentId + '\'' +
+                ", courses=" + Arrays.toString(courses) +
+                '}';
+    }
 }
