0349eb4ac2e8df174de5d43e3e741458d348f941
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-11-06 21:27:38 +0100

    Program optimization

--- MaxElementAndPrint/src/MaxElementAndPrint.java
+++ MaxElementAndPrint/src/MaxElementAndPrint.java
@@ -13,25 +13,22 @@ public class MaxElementAndPrint {
         // Declaration array of numbers
         int[] arrayOfNumbers = new int[N];
         int i=0;
+        int var=0;
         System.out.println("Enter element of array[range from 5 to 10]: ");
         while(i<N){
             System.out.println(i+1+". element: ");
             int retVal=scanner.nextInt();
             if(retVal>5 && retVal<=10){
                 arrayOfNumbers[i]=retVal;
+                if(arrayOfNumbers[i]>var)
+                    var=arrayOfNumbers[i];
                 i++;
             }else{
                 System.out.println("Element isn't in range! Enter again [range from 5 to 10]: ");
             }
         }
-        System.out.println("Elements of array:" + Arrays.toString(arrayOfNumbers));
-
-        int var=arrayOfNumbers[0];
-        for(int j=1;j<arrayOfNumbers.length;j++){
-            if(arrayOfNumbers[j]>var)
-                var=arrayOfNumbers[j];
-        }
         System.out.println("\nMax element is: " + var);
+
     }
 }
 
